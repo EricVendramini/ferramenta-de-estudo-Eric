a180811098baa744670fbd6c3e67043f2870184b
EricVendramini <eric.vendramini@escola.pr.gov.br>
2025-09-10 12:30:17 +0000

    fizemos cartoes no JS

---
+++ Perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'programação',
+    'Oque é HTML',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'Oque é CSS',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'Oque é JavaScript  ',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'Oque é HTML',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'Oque é HTML',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'Oque é HTML',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'Oque é HTML',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'Oque é HTML',
+    resposta
+)
+
+criaCartao(
+    'programação',
+    'Qual a diferença entre margin e padding?',
+    resposta
+)
