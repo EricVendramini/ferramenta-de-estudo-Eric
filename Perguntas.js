417e376f2ab904c93676e92cb946ea7f370b53e0
EricVendramini <eric.vendramini@escola.pr.gov.br>
2025-09-10 13:35:42 +0000

    10/09

--- Perguntas.js
+++ Perguntas.js
@@ -1,53 +1,53 @@
 criaCartao(
-    'programação',
-    'Oque é HTML',
-    resposta
+    'Programação',
+    'Oque é HTML?',
+    'O HTML é uma linguagem de programação que nos permite criar a estrutura do nosso site.'
 )
 
 criaCartao(
-    'programação',
-    'Oque é CSS',
-    resposta
+    'Programação',
+    'Oque é CSS?',
+    'É uma linguagem de programação utilizada para dar estilo á página.'
 )
 
 criaCartao(
-    'programação',
-    'Oque é JavaScript  ',
-    resposta
+    'Programação',
+    'Oque é JavaScript?',
+    'O JavaScript(JS) é uma linguagem de programação que nos permite interagir com a página.'
 )
 
 criaCartao(
-    'programação',
-    'Oque é HTML',
-    resposta
+    'Programação',
+    'Para que serve a tag article?',
+    'Para criar um conteudo ou elemento independente, no nosso caso estamos usando para criar cartões.'
 )
 
 criaCartao(
-    'programação',
-    'Oque é HTML',
-    resposta
+    'Programação',
+    'Oque é a tag footer?',
+    'É a tag utilizada pra criar o rodapé'
 )
 
 criaCartao(
-    'programação',
-    'Oque é HTML',
-    resposta
+    'Programação',
+    'Qual a diferença entre a tag title que usamos na tag head e a tag h1 que usamos na tag body?',
+    'A tag title aparecerá na aba da página que será aberta e a tag de título h1 é usada no corpo do site como título principal da página.'
 )
 
 criaCartao(
-    'programação',
-    'Oque é HTML',
-    resposta
+    'Programação',
+    'Qual a diferença entre as tags head e header?',
+    'Head é a tag de configurações, tudo que fizemos dentro dela, não aparecerá na página do nosso site. Header é tag de cabeçalho da página.'
 )
 
 criaCartao(
-    'programação',
-    'Oque é HTML',
-    resposta
+    'Programação',
+    'Para que serve o :root utilizado no CSS?',
+    'Serve para salvar variáveis para serem utilizadas durante o desenvolvimento da página, como por exemplo, cores e fontes.'
 )
 
 criaCartao(
-    'programação',
+    'Programação',
     'Qual a diferença entre margin e padding?',
-    resposta
+    'margin é o espaçamento externo enquanto o padding é o espaçamento interno.'
 )
